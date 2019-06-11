03c1d9005d5837a9a7e7a8b509baae46daf35632
ZxZ <mattias.langseth98@gmail.com>
2019-06-11 18:31:34 +0200

    Create setup.bat

---
+++ src/batch/setup.bat
@@ -0,0 +1,19 @@
+@title Casino Neat Diceroller ~ Setup
+@echo off
+cls
+set /p token=Please enter your bot token:
+echo Setting up directories ...
+mkdir enc
+mkdir DB
+echo Done!
+echo Creating token file ...
+cd enc
+@echo %token% > token.cncrypt
+cd ..
+echo Done!
+echo Setting up DB ...
+cd DB
+type NUL > database.cndb
+type NUL > tmp.cncrypt
+cd ..
+echo Done!
