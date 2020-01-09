66c04357bcf060a5b2ea5c40c8fae51080bf28d2
ZxZ <mattias.langseth98@gmail.com>
2020-01-09 18:06:45 +0100

    Removed unused import

--- src/RPG.py
+++ src/RPG.py
@@ -12,8 +12,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-from cn_globals import *
+""" 
 import discord
 import random
 from colorama import init
